f5e42a16e8fecda38f03edec44a6b26f5af04532
atpking <atpking@gmail.com>
2016-06-07 16:33:40 +0800

    fix beta deploy config

--- config/beta.rb
+++ config/beta.rb
@@ -2,6 +2,5 @@ set :deploy_user, 'deploy'
 set :stage, :beta
 set :branch, 'develop'
 set :rails_env, :production
-server '192.168.10.4', user: 'deploy', roles: %w(web app db), primary: true
-server '192.168.10.5', user: 'deploy', roles: %w(web app db), primary: true
-server '192.168.10.6', user: 'deploy', roles: %w(web app db), primary: true
+server '192.168.10.7', user: 'deploy', roles: %w(web app db), primary: true
+server '192.168.10.8', user: 'deploy', roles: %w(web app db), primary: true
